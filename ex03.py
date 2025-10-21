3aadc33220c2c1ac04506838643e994eebfd40a7
unitedivan <168294901+unitedivan@users.noreply.github.com>
2025-10-21 19:48:29 -0400

    Add files via upload

---
+++ ex03.py
@@ -0,0 +1,15 @@
+a = 5
+b = 6
+c = 10
+
+print("a + b + c = ", a + b + c)
+print("a * b + c = ", a * b + c)
+print("a * b * c = ", a * b * c)
+print("c - b - a = ", c - b - a)
+print("a * b / c = ", a * b / c)
+print()
+
+print("University of Massachusetts at Boston")
+print("100 Morrissey Boulevard")
+print("Boston, Massachusetts")
+print("02125-3393")
